4255b385f1dd7e01e653f0cf1b25572d888576fd
JustynLim <89184180+JustynLim@users.noreply.github.com>
2025-06-23 23:51:33 +0800

    Recreated build

--- React-App/src/index.js
+++ React-App/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 
-const root = ReactDOM.render(<App />, document.getElementById('root'));
-root.render(<App />)
-// ReactDOM.render(<App />, document.getElementById('root'));
+// const root = ReactDOM.render(<App />, document.getElementById('root'));
+// root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('root'));
